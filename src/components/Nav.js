52d95c19e7e4fa0fef5d829b913f35f2114324db
github username <github email address>
2018-02-25 19:01:34 -0500

    more styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,20 +10,20 @@ const Nav = (props) => {
       <div className='Search'>
         <p className='Search-by-text'>SEARCH BY:</p>
         <select className='Search-selection' onChange={props.updateFilterSelection} name="" id="">
-          <option value="title">TITLE</option>
-          <option value="artist">ARTIST</option>
-          <option value="museum">MUSEUM</option>
+          <option value="title">•TITLE•</option>
+          <option value="artist">•ARTIST•</option>
+          <option value="museum">•MUSEUM•</option>
         </select>
         <input className='Search-input' onChange={props.updateFilterText} type="text"/>
       </div>
       <div className='Sort'>
         <div className='Sort-title'>
           <img className='Sort-icon' onClick={props.toggleTitleSort} src={sortIcon} alt="^"/>
-          <p onClick={props.toggleTitleSort}>TITLE</p>
+          <a href='#' onClick={props.toggleTitleSort}>TITLE</a>
         </div>
         <div className='Sort-year'>
           <img className='Sort-icon' onClick={props.toggleYearSort} src={sortIcon} alt="^"/>
-          <p onClick={props.toggleYearSort}>YEAR</p>
+          <a href='#' onClick={props.toggleYearSort}>YEAR</a>
         </div>
       </div>
     </div>
