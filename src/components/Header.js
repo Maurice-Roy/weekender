fc1fed55bcea6e9bb065a976d7df6ba87e2d2442
github username <github email address>
2018-02-26 11:25:35 -0500

    added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import logo from '../logo.svg'
+import logo from '../scull.png'
 
 const Header = (props) => {
   return (
     <div>
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <h1 className="App-title">- ART BY DEAD PEOPLE -</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">♱ ART BY DEAD PEOPLE ♱</h1>
       {/* <header className="App-header">
         <img src='' className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
